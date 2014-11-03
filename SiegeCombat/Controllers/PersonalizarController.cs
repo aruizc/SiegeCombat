dec97e7e17f4ca309f9de4662d2ce3b2642b9f55
SOAD41 <darthbeider@hotmail.com>
2014-11-03 17:44:28 -0600

    Personalizar

--- SiegeCombat/Controllers/PersonalizarController.cs
+++ SiegeCombat/Controllers/PersonalizarController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SiegeCombat.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,11 +11,41 @@ namespace SiegeCombat.Controllers
     {
         //
         // GET: /Personalizar/
-
+        SiegeCombat.Models.SiegeCombatEntities bd = new Models.SiegeCombatEntities();
         public ActionResult Index()
         {
             return View();
         }
 
+
+        public ActionResult CambiarImagen(string logo)
+         {
+             try
+             {
+                 Jugador jugador = (Jugador)Session["Jugador"];
+                 jugador.Imagen = logo;
+
+                 bd.Entry(jugador).State = System.Data.EntityState.Modified;
+                 bd.SaveChanges();
+             }
+             catch (Exception ex)
+             {
+              return null;
+             }
+         }
+
+        public ActionResult CambiarNickname(string nickname)
+        {
+            try
+            {
+                Jugador jugador = (Jugador)Session["Jugador"];
+                bd.Entry(jugador).State = System.Data.EntityState.Modified;
+                bd.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
     }
 }
