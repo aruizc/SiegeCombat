4b92dd91a2cc76fef477ab5b4b8586e460ffa8ca
SOAD41 <darthbeider@hotmail.com>
2014-12-08 12:00:03 -0600

    Tienda

--- SiegeCombat/Controllers/TiendaController.cs
+++ SiegeCombat/Controllers/TiendaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SiegeCombat.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,10 +11,12 @@ namespace SiegeCombat.Controllers
     {
         //
         // GET: /Tienda/
-
+        SiegeCombatEntities bd = new SiegeCombatEntities();
         public ActionResult Index()
         {
-            return View();
+            Usuario usuario = (Usuario)Session["Usuario"];
+            Jugador jugador = bd.Jugador.Find(usuario.IdJugador);
+            return View(jugador);
         }
 
         public ActionResult ComprarMejora(int mejora)
@@ -21,13 +24,23 @@ namespace SiegeCombat.Controllers
             bool valido = false;
             try
             {
-
+                Usuario usuario = (Usuario)Session["Usuario"];
+                Jugador jugador = bd.Jugador.Find(usuario.IdJugador);
+                if (mejora == 1)
+                    jugador.Mejora1 = true;
+                else if (mejora == 2)
+                    jugador.Mejora2 = true;
+                else if (mejora == 3)
+                    jugador.Mejora3 = true;
+                bd.Entry(jugador).State = System.Data.EntityState.Modified;
+                bd.SaveChanges();
+                valido = true;
             }
             catch (Exception ex)
             {
                
             }
-            return null;
+            return Json(valido);
         }
     }
 }
